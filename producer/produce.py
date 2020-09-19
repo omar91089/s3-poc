0d46bc6b4e64e2ae59b55c263d008d30f30ca858
Omar Hasan <omar91089@gmail.com>
2020-09-19 14:32:33 +0530

    adding transfer strategy to producer

--- producer/produce.py
+++ producer/produce.py
@@ -2,19 +2,48 @@
 # alternate between using consumer API and message queue to give file metadata
 
 import boto3
-import random
+import logging
+import redis
+import requests
 import time
 
-WAIT_TIME = 60  # seconds
+CONSUMER_URL = 'http://localhost/process-file/'
+PRODUCE_WAIT_TIME = 60  # seconds
+S3_BUCKET_NAME = 'tc-app-integration-testing'
+S3_KEY_NAME = 'omar_testing/'
+
+redis_client = redis.Redis()
+logger = logging.getLogger(__file__)
+
+
+class TransferStrategy:
+    def __init__(self):
+        self._state = False
+
+    def rest_api_strategy(self):
+        r = requests.post(CONSUMER_URL)
+
+    def message_queue_strategy(self):
+        redis_client.publish()
+
+    def send_for_processing(self, file_name):
+        if self._state:
+            self.rest_api_strategy()
+        else:
+            self.message_queue_strategy()
+        self._state = not self._state
 
 
 def upload_file_to_s3(file_name):
-    s3_url = ''
-    return s3_url
+    s3_client = boto3.client('s3')
+    try:
+        s3_client.upload_file(file_name, S3_BUCKET_NAME, S3_KEY_NAME + file_name)
+    except Exception as err:
+        logger.error('Problem in uploading file to S3', err)
 
 
 def generate_file():
-    file_name = ''
+    file_name = r'C:\Users\omar9\Desktop\tika_drf_s3.png'
     return file_name
 
 
@@ -23,11 +52,12 @@ def send_for_processing(file_name):
 
 
 def produce():
+    transfer_strategy = TransferStrategy()
     while True:
         file_name = generate_file()
         upload_file_to_s3(file_name)
-        send_for_processing(file_name)
-        time.sleep(WAIT_TIME)
+        transfer_strategy.send_for_processing(file_name)
+        time.sleep(PRODUCE_WAIT_TIME)
 
 
 if __name__ == '__main__':
